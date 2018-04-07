1f024b38c7d311f8d63bbee9fbfb3f4330e242d4
Vikram Reddy <vreddy@ischool.berkeley.edu>
2018-04-07 14:51:26 -0700

    db to prepare for merge

---
+++ server/db.js
@@ -0,0 +1,126 @@
+///// db stuff
+
+var connectionName = 'capai-198619:us-west1:capsql';
+var rootPwd = 'CaptivateAI';
+
+// Replace these variables in this block with real values.
+var user = 'user_name';
+var userPwd = 'user_password';
+
+//name of db
+var db = 'content';
+
+var root = 'root';
+var instanceUrl = 'jdbc:google:mysql://' + connectionName;
+var dbUrl = instanceUrl + '/' + db;
+
+
+function readQuotes() {
+    var conn = Jdbc.getCloudSqlConnection(dbUrl, root, rootPwd);
+    var stmt = conn.createStatement();
+    stmt.setMaxRows(1000);
+
+    var results = stmt.executeQuery('SELECT * FROM quotes');
+
+    quotes = [];
+
+    while (results.next()) {
+        var quoteString = '"' + results.getString(2) + '" -' + results.getString(1);
+        quotes.push(quoteString);
+    }
+
+    results.close();
+    stmt.close();
+
+    return quotes;
+}
+
+// functions below from how-to-guide
+
+// Create a new database within a Cloud SQL instance.
+function createDatabase(db_name) {
+    var conn = Jdbc.getCloudSqlConnection(instanceUrl, root, rootPwd);
+    conn.createStatement().execute('CREATE DATABASE ' + db_name);
+}
+
+// Create a new user for your database with full privileges.
+function createUser() {
+    var conn = Jdbc.getCloudSqlConnection(dbUrl, root, rootPwd);
+
+    var stmt = conn.prepareStatement('CREATE USER ? IDENTIFIED BY ?');
+    stmt.setString(1, user);
+    stmt.setString(2, userPwd);
+    stmt.execute();
+
+    conn.createStatement().execute('GRANT ALL ON `%`.* TO ' + user);
+}
+
+// Create a new table in the database.
+function createTable() {
+    var conn = Jdbc.getCloudSqlConnection(dbUrl, root, rootPwd);
+    conn.createStatement().execute('CREATE TABLE quotes '
+        + '(author VARCHAR(255), quote VARCHAR(2000), '
+        + 'entryID INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(entryID));');
+}
+
+////////// write
+
+// Write one row of data to a table.
+function writeOneRecord(auth, quo) {
+    var conn = Jdbc.getCloudSqlConnection(dbUrl, root, rootPwd);
+
+    var stmt = conn.prepareStatement('INSERT INTO quotes '
+        + '(author, quote) values (?, ?)');
+    stmt.setString(1, auth);
+    stmt.setString(2, quo);
+    stmt.execute();
+}
+
+// Write 500 rows of data to a table in a single batch.
+function writeManyRecords() {
+    var conn = Jdbc.getCloudSqlConnection(dbUrl, user, userPwd);
+    conn.setAutoCommit(false);
+
+    var start = new Date();
+    var stmt = conn.prepareStatement('INSERT INTO entries '
+        + '(guestName, content) values (?, ?)');
+    for (var i = 0; i < 500; i++) {
+        stmt.setString(1, 'Name ' + i);
+        stmt.setString(2, 'Hello, world ' + i);
+        stmt.addBatch();
+    }
+
+    var batch = stmt.executeBatch();
+    conn.commit();
+    conn.close();
+
+    var end = new Date();
+    Logger.log('Time elapsed: %sms for %s rows.', end - start, batch.length);
+}
+
+///////////////////// read
+
+// Read up to 1000 rows of data from the table and log them.
+function readFromTable() {
+    var conn = Jdbc.getCloudSqlConnection(dbUrl, user, userPwd);
+
+    var start = new Date();
+    var stmt = conn.createStatement();
+    stmt.setMaxRows(1000);
+    var results = stmt.executeQuery('SELECT * FROM entries');
+    var numCols = results.getMetaData().getColumnCount();
+
+    while (results.next()) {
+        var rowString = '';
+        for (var col = 0; col < numCols; col++) {
+            rowString += results.getString(col + 1) + '\t';
+        }
+        Logger.log(rowString)
+    }
+
+    results.close();
+    stmt.close();
+
+    var end = new Date();
+    Logger.log('Time elapsed: %sms', end - start);
+}
