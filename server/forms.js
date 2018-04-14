a1921a85042015bb2ba4bb5128086ce8615e9a31
Vikram Reddy <vreddy@ischool.berkeley.edu>
2018-04-14 11:01:19 -0700

    removed func from forms api, added jquery helpers

--- server/forms.js
+++ server/forms.js
@@ -12,11 +12,12 @@ function processForm(formObject) {
 /* 
  * Function to process Form input from the client side.
  */
-function processBrainstormUsingForm(formObject) {
-  Logger.log("I am here!!! Process away!");
-  console.log("I am here!!! Process away!");
-  // blob will be encoded as a string
-  var formBlob = formObject.brainstorm;
-  var keywords = analyzeText(formBlob);
-  return keywords;
-}
+// repeated func here, caused errors
+// function processBrainstormUsingForm(formObject) {
+//   Logger.log("I am here!!! Process away!");
+//   console.log("I am here!!! Process away!");
+//   // blob will be encoded as a string
+//   var formBlob = formObject.brainstorm;
+//   var keywords = analyzeText(formBlob);
+//   return keywords;
+// }
